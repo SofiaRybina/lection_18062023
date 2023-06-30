50e4c4a4e4e232af5dd4f8c756c0ade48b0db823
Sofia <rybina.sofia@gmail.com>
2023-06-30 23:36:59 +0300

    Вывод с разъяснением

--- example_max_elem_massive/Program.cs
+++ example_max_elem_massive/Program.cs
@@ -16,31 +16,36 @@ void PrintArr(int[] array)
     Console.WriteLine();
 }
 
+
 void SelectionSort(int[] array)
 {
-    for(int i = 0; i < array.Length - 1; i++)
+    for(int i = 0; i < array.Length - 1; i++) //0...8
     {
-        //Console.WriteLine($"i->{i}");
-        //Console.WriteLine($"{array.Length}");
+        Console.WriteLine($"i->{i}");
+        Console.WriteLine($"array.Length ={array.Length}");
         int minposition = i;
-        //Console.WriteLine($"minposition->{minposition}");
-        for(int j = i + 1; j < array.Length; j++)
+        Console.WriteLine($"minposition->{minposition}");
+        for(int j = i + 1; j < array.Length; j++) //1...9
         {
-            //Console.WriteLine($"j={j}");
-            //Console.WriteLine($"if->{array[j]}<{array[minposition]}");
+            Console.WriteLine($"j={j}");
+            Console.WriteLine($"array[j]->{array[j]}<array[minposition]->{array[minposition]}");
             if(array[j]< array[minposition])
             {
-                //Console.WriteLine($"if->{array[j]}<{array[minposition]}");
+                Console.WriteLine($"if->{array[j]}<{array[minposition]}");
                 minposition = j;
+                Console.WriteLine($"minposition->{minposition}");
+
             }
         }
         int temp = array[i];
-        //Console.WriteLine($"temp={temp}");
+        Console.WriteLine($"temp={temp}");
         array[i] = array[minposition];
-        //Console.WriteLine($"array[i]={array[i]}");
+        Console.WriteLine($"array{i}={array[i]}");
         array[minposition] = temp;
-        //Console.WriteLine($"array[minposition]={array[minposition]}");
+        Console.WriteLine($"array{minposition}={array[minposition]}");
+        Console.WriteLine();
     }
+
 }
 
 PrintArr(arr);
